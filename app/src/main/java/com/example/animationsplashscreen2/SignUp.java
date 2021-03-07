8d8cf9d56dd60b83b8f4436132648b0e828c98cc
Saroj Kumar Pradhan <sarojkp.2k@gmail.com>
2021-03-07 20:19:11 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/animationsplashscreen2/SignUp.java
@@ -0,0 +1,57 @@
+package com.example.animationsplashscreen2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class SignUp extends AppCompatActivity {
+
+    TextInputLayout regName,regUsername,regEmail,regPhoneNo,regPassword;
+    Button regBtn,regToLoginBtn;
+
+    FirebaseDatabase rootNode;
+    DatabaseReference reference;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_sign_up);
+
+        regName = findViewById(R.id.reg_name);
+        regUsername = findViewById(R.id.reg_username);
+        regEmail = findViewById(R.id.reg_email);
+        regPhoneNo = findViewById(R.id.reg_phoneNo);
+        regPassword = findViewById(R.id.reg_password);
+        regBtn = findViewById(R.id.reg_btn);
+        regToLoginBtn = findViewById(R.id.reg_login_btn);
+
+
+        regBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                rootNode = FirebaseDatabase.getInstance();
+                reference = rootNode.getReference("users");
+
+
+                //get all the values
+                String name = regName.getEditText().getText().toString();
+                String username = regUsername.getEditText().getText().toString();
+                String email = regEmail.getEditText().getText().toString();
+                String phoneNo = regPhoneNo.getEditText().getText().toString();
+                String password = regPassword.getEditText().getText().toString();
+                UserHelperClass helperClass = new UserHelperClass(name,username,email,phoneNo,password);
+
+                reference.child(phoneNo).push().setValue(helperClass);
+            }
+        });
+    }
+}
